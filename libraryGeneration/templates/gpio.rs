c8eede9b574a0ecb2d5b0f287b950875d3763988
Elana Courtines <courtines.e@gmail.com>
2024-01-30 11:19:08 +0100

    Fix(libgen): missing imports in template files

--- libraryGeneration/templates/gpio.rs
+++ libraryGeneration/templates/gpio.rs
@@ -1,5 +1,7 @@
 {%- import "gpio_macro.rs" as gpiomacro %}
-
+extern crate core;
+use crate::core::ptr::write_volatile;
+use crate::core::ptr::read_volatile;
 use crate::stm32rustlib::various;
 {% include "address.rs" %}
 
