8660570cb7c6f7df185f13c4c588d4d28fc742a7
Elana Courtines <courtines.e@gmail.com>
2024-01-31 17:54:05 +0100

    Update(libgen): nvic and tim json and templates

--- libraryGeneration/templates/nvic.rs
+++ libraryGeneration/templates/nvic.rs
@@ -11,3 +11,13 @@ use crate::stm32rustlib::various;
 {{nvicmacro.gen_nvic_set(component.name, register.name)}}
     {%- endfor %}
 {%- endfor -%}
+
+{%- for constant in constants %}
+    {%- if constant.name == "NVIC_IRQ" %}
+pub fn nvic_handler_set(vector: u32, f: u32){
+    unsafe {
+        write_volatile(({{constant.value}} + vector * 4) as *mut u32, f);
+    }
+}
+    {%- endif -%}
+{%- endfor %}
