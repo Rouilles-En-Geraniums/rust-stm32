94bf4df7785bc36f5dcc09952fb4e0ff42700c08
Elana Courtines <courtines.e@gmail.com>
2024-02-01 11:13:25 +0100

    Clean(libgen): blink and button unitary tests

--- libraryGeneration/unitaryTests/blink.rs
+++ libraryGeneration/unitaryTests/blink.rs
@@ -3,47 +3,28 @@
 extern crate core;
 use panic_halt as _;
 use cortex_m_rt::entry;
-
-use crate::core::ptr::write_volatile;
-use crate::core::ptr::read_volatile;
+use cortex_m::asm;
 
 use crate::stm32rustlib::gpio;
+use crate::stm32rustlib::rcc;
+use crate::stm32rustlib::various;
+
 pub mod stm32rustlib;
-
-
-const RCC_AHB1ENR : u32 = 0x40023830 ;
-pub fn rcc_write(value: u32) {
-    unsafe {
-        write_volatile( RCC_AHB1ENR as *mut u32, value )
-    };
-}
-pub fn rcc_read() -> u32 {
-    unsafe {
-        read_volatile( RCC_AHB1ENR as *mut u32 )
-    }
-}
-
 
 #[entry]
 fn main() -> ! {
-    rcc_write(rcc_read() | (1 << 3));
+    rcc::rcc_ahb1enr_write(rcc::rcc_ahb1enr_read() | (1 << 3)); //GPIO D
     
-    let my_led = ('D', 2);
+    let my_led = ('D', 12); // Built-in green led
 
-    gpio::gpiod_moder_write(1 << (my_led.1*2));
-    // GPIOD_OTYPER = GPIOD_OTYPER & ~( 1 << (GREEN_LED));
+    gpio::gpiod_moder_write(various::rep_bits(gpio::gpiod_moder_read(), my_led.1*2, 2, gpio::GPIO_MODER_OUT));
 
-    let mut i;
+    let mut bstate = RELEASED;
+    gpio::digital_write(my_led, various::LOW);
     loop {
-        gpio::gpiod_bsrr_write(1 << my_led.1);
-        i = 0;
-        while i < 3000000 {
-            i+=1;
-        }
-        gpio::gpiod_bsrr_write(1 << (my_led.1 + 16));
-        i = 0;
-        while i < 3000000 {
-            i+=1;
-        }
+		gpio::digital_write(my_led, various::LOW);
+		asm::delay(4200000);
+		gpio::digital_write(my_led, various::HIGH);
+		asm::delay(4200000);
     }
 }
