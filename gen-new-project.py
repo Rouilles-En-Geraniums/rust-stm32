0b6d67294bb1e273b69121daa58d6c478369d875
Elana Courtines <courtines.e@gmail.com>
2024-02-12 16:54:20 +0100

    Style(libgen): cleanup comments

--- gen-new-project.py
+++ gen-new-project.py
@@ -42,7 +42,8 @@ def main():
     # Parse argument line
     args = cmdlineParse()
 
-    os.system("cargo new --edition 2018 --bin {}".format(args.projectname))
+    # Create new project
+    os.system("cargo new --bin {}".format(args.projectname))
 
     # .cargo/config.toml
     config_dest = args.projectname + "/.cargo/config.toml"
@@ -55,8 +56,8 @@ def main():
         output_file.write("[build]\n")
         output_file.write("target = \"{}\"\n".format(args.target))
 
+
     # Src folder
-    
     # main.rs
     main_orig = "cross-compilation/src/main.rs"
     main_file = args.projectname + "/src/main.rs"
@@ -66,46 +67,44 @@ def main():
     os.system("(cd libraryGeneration/libgen && python3 main.py -j ../descriptionFiles/stm32f407/*.json -e ../descriptionFiles/stm32f407/*.rs -o ../../{}/src/stm32rustlib)".format(args.projectname))
 
 
-    # toml
+    # Cargo.toml
     toml_file = args.projectname + "/Cargo.toml"
-
     with open(toml_file, 'w') as output_file:
         output_file.write("[package]\n")
         output_file.write("authors = [\"{}\"]\n".format(args.author))
-        output_file.write("edition = \"2018\"\n")
+        output_file.write("edition = \"2021\"\n")
         output_file.write("readme = \"README.md\"\n")
         output_file.write("name = \"{}\"\n".format(args.projectname))
         output_file.write("version = \"0.1.0\"\n\n")
         output_file.write("[dependencies]\n")
         output_file.write("geranium_rt = { path = \"../geranium_rt\" }\n")
 
+
     # Makefile
     makefile_orig = "cross-compilation/Makefile"
     makefile_file = args.projectname + "/Makefile"
     shutil.copyfile(makefile_orig, makefile_file)
 
-    # Read in the file
     with open(makefile_file, 'r') as file:
         filedata = file.read()
-
-    # Replace the target string
     filedata = filedata.replace('TARGET =', 'TARGET = {}'.format(args.target))
     filedata = filedata.replace('GDB =', 'GDB = {}'.format(args.gdb))
     filedata = filedata.replace('APP =', 'APP = {}'.format(args.projectname))
-
-    # Write the file out again
     with open(makefile_file, 'w') as file:
         file.write(filedata)
+
 
     # Openocd
     openocdcfg_orig = "cross-compilation/openocd.cfg"
     openocdcfg_file = args.projectname + "/openocd.cfg"
     shutil.copyfile(openocdcfg_orig, openocdcfg_file)
 
+
     # GDBinit
     gdbinit_orig = "cross-compilation/gdbinit"
     gdbinit_file = args.projectname + "/gdbinit"
     shutil.copyfile(gdbinit_orig, gdbinit_file)
+
     
     # README
     readme_orig = "cross-compilation/README.md"
