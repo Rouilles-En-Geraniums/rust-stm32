9a982151349cf5387cb32987a959188cfa45a42b
Elana Courtines <courtines.e@gmail.com>
2024-02-12 13:02:16 +0100

    Update(libgen): timer unitary test

--- libraryGeneration/unitaryTests/timer.rs
+++ libraryGeneration/unitaryTests/timer.rs
@@ -11,39 +11,10 @@ extern crate core;
 use core::arch::asm;
 
 pub mod stm32rustlib;
-use crate::stm32rustlib::adc::*;
-use crate::stm32rustlib::dac::*;
-use crate::stm32rustlib::exti::*;
 use crate::stm32rustlib::gpio::*;
-use crate::stm32rustlib::nvic::*;
 use crate::stm32rustlib::rcc::*;
-use crate::stm32rustlib::tim::*;
 use crate::stm32rustlib::various::*;
-
-const APB1_CLK: u32 = 42_000_000;
-
-#[no_mangle]
-fn mywait(t: u32) { // milliseconds
-
-    let psc: u32 = APB1_CLK / 1000;
-    let period: u32 = t;
-
-    rcc_apb1enr_write(rcc_apb1enr_read() | (1 << 2)); //tim4en
-
-    tim4_cr1_write(tim4_cr1_read() & !TIM_CEN);
-    tim4_psc_write(psc - 1);
-    tim4_arr_write(period);
-    tim4_egr_write(TIM_UG);
-    tim4_sr_write(0);
-    tim4_cr1_write(tim4_cr1_read() | TIM_CEN);
-    
-    while (tim4_sr_read() & TIM_UIF) == 0 {};
-
-    tim4_sr_write(0);
-    tim4_cr1_write(tim4_cr1_read() & !TIM_CEN);
-}
-
-
+use crate::stm32rustlib::wait::*;
 
 
 #[no_mangle]
@@ -56,8 +27,8 @@ fn main() {
     digital_write(my_led, LOW);
     loop {
         digital_write(my_led, LOW);
-        mywait(1000);
+        wait_ms(1000);
         digital_write(my_led, HIGH);
-        mywait(1000);
+        wait_ms(1000);
     }
 }
