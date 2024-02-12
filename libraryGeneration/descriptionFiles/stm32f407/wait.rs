69a5b23218e52c3b607a7ce8becdf2dd403b22d7
Elana Courtines <courtines.e@gmail.com>
2024-02-12 13:02:50 +0100

    Update(libgen): arming timer in wait.rs

--- libraryGeneration/descriptionFiles/stm32f407/wait.rs
+++ libraryGeneration/descriptionFiles/stm32f407/wait.rs
@@ -28,3 +28,32 @@ pub fn wait_ms(t: u32) { // milliseconds
     tim2_sr_write(0);
     tim2_cr1_write(tim2_cr1_read() & !TIM_CEN);
 }
+
+pub fn timer_arm(t: u32)  { // milliseconds
+    let psc: u32 = APB1_CLK / 1000;
+    let period: u32 = t;
+
+    rcc_apb1enr_write(rcc_apb1enr_read() | (1 << 0)); //tim2en
+
+    tim2_cr1_write(tim2_cr1_read() & !TIM_CEN);
+    tim2_psc_write(psc - 1);
+    tim2_arr_write(period);
+    tim2_egr_write(TIM_UG);
+    tim2_sr_write(0);
+    tim2_cr1_write(tim2_cr1_read() | TIM_CEN);
+}
+
+pub fn timer_timeout(){
+    while (tim2_sr_read() & TIM_UIF) == 0 {};
+
+    tim2_sr_write(0);
+    tim2_cr1_write(tim2_cr1_read() & !TIM_CEN);
+}
+
+pub fn timer_isTimeout() -> bool {
+    if (tim2_sr_read() & TIM_UIF) == 0 {
+        false
+    } else {
+        true
+    }
+}
