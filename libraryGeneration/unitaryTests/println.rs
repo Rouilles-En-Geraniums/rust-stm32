3c74e2ff1d06b68a72f72d1e796b335db7046e8e
TwilCynder <twilcynder@gmail.com>
2024-02-16 14:47:10 +0100

    Chore : merge second commit

--- libraryGeneration/unitaryTests/println.rs
+++ libraryGeneration/unitaryTests/println.rs
@@ -1,42 +1,58 @@
 #![no_std]
 #![no_main]
+/**
+ *	Rust on STM32 Project by Rouilles en GeraniumTM
+ *	Copyright (C) 2024 Université de Toulouse :
+ *   - Oussama Felfel - oussama.felfel@univ-tlse3.fr
+ *   - François Foltete - francois.foltete@univ-tlse3.fr
+ *   - Elana Courtines - elana.courtines@univ-tlse3.fr
+ *   - Teo Tinarrage - teo.tinarrage@univ-tlse3.fr
+ *   - Zineb Moubarik - zineb.moubarik@univ-tlse3.fr
+ *
+ *  This library aims to provide the following :
+ *   - a rust library generation tool to safely access memory ;
+ *   - a support to flash STM32 boards ;
+ *   - a task scheduling tool that generates the associated rust code.
+ *
+ *  The development of this library has done as a Proof of Concept and
+ *  is currently only tested for STM32F407-G DISC1 Boards.
+ *
+ *  It is our hope that using this library to enable development on
+ *  other boards will be facilitated.
+ *
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+**/
 
+// The following creates are required to use println
 extern crate geranium_rt;
-
-use geranium_rt::stm32rustlib::gpio::*;
-use geranium_rt::stm32rustlib::rcc::*;
-use geranium_rt::stm32rustlib::various::*;
-use geranium_rt::stm32rustlib::wait::*;
-
-// TODO you need these includes to use println
-use geranium_rt::stm32rustlib::geranium_print::*;
 extern crate core;
+use geranium_rt::stm32rustlib::geranium_print::*;
 use core::format_args;
 
 // TODO you can impl Debug trait to use it
-// we think would also work with Display but it hasn't been tested
+// it could also work with Display but it hasn't been tested
 #[derive(Debug)]
 struct TupleStruct(char, u32);
 
 #[no_mangle]
 fn main() {
-    wait_init_timers();
-
-    rcc_ahb1enr_write(rcc_ahb1enr_read() | RCC_AHB1ENR_GPIODEN);
 
     let my_led = ('D', 12); // Built-in green led
-    gpiod_moder_write(rep_bits(gpiod_moder_read(), my_led.1*2, 2, GPIO_MODER_OUT));
-
     let my_ledst = TupleStruct('A', 323_u32);
-    // TODO you need to use format_args! macro to println
+
+    // You must use the format_args! macro to println
     println(format_args!("my_led: ({},{})", my_led.0, my_led.1));
     println(format_args!("my_ledst: {my_ledst:?}"));
 
-    digital_write(my_led, LOW);
     loop {
-        digital_write(my_led, LOW);
-        wait_ms(1000);
-        digital_write(my_led, HIGH);
-        wait_ms(1000);
     }
 }
