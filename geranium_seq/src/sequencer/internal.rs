5b526bf7b54069c4d2f63e12bd452e747b7a65e4
francois <>
2024-02-19 13:26:20 +0100

    Draft(task-seq/dev): Job have a reference to OrdoTaskj
    Replace task_index (index to an array of OrdoTask)
    by ordo_task wich is a reference to a RefCell<OrdoTask>

--- geranium_seq/src/sequencer/internal.rs
+++ geranium_seq/src/sequencer/internal.rs
@@ -7,14 +7,13 @@ use geranium_rt::stm32rustlib::delay::delay_ms;
 
 
 pub struct OrdoTask <'a>{
-    pub name: *const str,
     pub task: &'a mut dyn Task
 }
 
-pub struct Job{
-    pub task_index: usize,
+pub struct Job<'a>{
+    pub ordo_task: &'a RefCell<OrdoTask<'a>>,
     pub start: u32,
-    pub duration: u32
+    pub duration: u32 // TODO check if wait is inclusive or exclusive
 }
 
 pub fn run_task(ordo_task: &mut OrdoTask, max_time: u32){
@@ -23,32 +22,21 @@ pub fn run_task(ordo_task: &mut OrdoTask, max_time: u32){
     timer_timeout();
 }
 
-pub fn run_sequencer(ordo_tasks: &mut [OrdoTask], jobs: &[Job], hyperperiod: u32) -> !{
-    for task in ordo_tasks.iter_mut() {
-        //println!("{}", task.name);
-        task.task.init();
-    }
+pub fn run_sequencer(jobs: &[Job], hyperperiod: u32) -> !{
+    if jobs.is_empty() { loop {} }
 
     loop {
-        if jobs.len() < 1 {
-            continue;
-        }
-
         delay_ms(jobs[1].start);
 
         let mut i: usize = 0;
         while i < jobs.len() - 1 {
             let job = &jobs[i];
             let next_job = &jobs[i + 1];
-            let ordo_task: &mut OrdoTask = &mut ordo_tasks[job.task_index];
-    
-            run_task(ordo_task, next_job.start - job.start);
-    
+            run_task(&mut job.task.borrow_mut(), next_job.start - job.start);
+
             i += 1;
         }
         let job = &jobs[i];
-        let ordo_task: &mut OrdoTask = &mut ordo_tasks[job.task_index];
-    
-        run_task(ordo_task, hyperperiod - job.start);
+        run_task(&mut job.task.borrow_mut(), hyperperiod - job.start);
     }
 }
