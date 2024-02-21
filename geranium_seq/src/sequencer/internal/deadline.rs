02da594b78e2088eb8aef9fbfa2ea0cc4b16c63e
francois <>
2024-02-21 16:00:32 +0100

    Revert "Fix : use seq_delay_ms instead of the old delay_ms in deadline.rs"
    This reverts commit c107f80c344580d9a234327599b729e3cb3242b4.

--- geranium_seq/src/sequencer/internal/deadline.rs
+++ geranium_seq/src/sequencer/internal/deadline.rs
@@ -32,8 +32,11 @@ pub fn run_sequencer(jobs: &[Job], hyperperiod: u32) -> !{
         let job = &jobs[0];
         loop {
             seq_delay_ms(job.start);
-            run_task(&mut job.ordo_task.borrow_mut(), job.ordo_task.borrow_mut().duration);
-            seq_delay_ms(hyperperiod - (job.start + job.ordo_task.borrow_mut().duration ));
+            let mut ordo_task = job.ordo_task.borrow_mut();
+            let duration = ordo_task.duration;
+
+            run_task(&mut ordo_task, duration);
+            seq_delay_ms(hyperperiod - (job.start + duration));
         }
     }
 
@@ -45,14 +48,20 @@ pub fn run_sequencer(jobs: &[Job], hyperperiod: u32) -> !{
         while i < jobs.len() - 1 {
             let job = &jobs[i];
             let next_job = &jobs[i + 1];
-            run_task(&mut job.ordo_task.borrow_mut(), job.ordo_task.borrow_mut().duration);
-            seq_delay_ms(next_job.start - (job.start + job.ordo_task.borrow_mut().duration));
+            let mut ordo_task = job.ordo_task.borrow_mut();
+            let duration = ordo_task.duration;
+
+            run_task(&mut ordo_task, duration);
+            seq_delay_ms(next_job.start - (job.start + duration));
 
             i += 1;
         }
         let job = &jobs[i];
-        run_task(&mut job.ordo_task.borrow_mut(), job.ordo_task.borrow_mut().duration);
-        seq_delay_ms(hyperperiod - (job.start + job.ordo_task.borrow_mut().duration));
+        let mut ordo_task = job.ordo_task.borrow_mut();
+        let duration = ordo_task.duration;
+
+        run_task(&mut ordo_task, duration);
+        seq_delay_ms(hyperperiod - (job.start + duration));
     }
 }
 
